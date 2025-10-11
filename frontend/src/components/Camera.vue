e4fcda9b8bafcf2e27eac750e3765762a1ae8757
gitbani <banisomo2003@gmail.com>
2025-10-10 23:14:49 -0400

    Add camera

---
+++ frontend/src/components/Camera.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+  const videoEl = document.getElementById('video') as HTMLMediaElement;
+
+  let stream = null;
+  try {
+    stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
+    videoEl.srcObject = stream;
+  } catch (error) {
+    console.error(error);
+  }
+})
+
+</script>
+
+<template>
+  <video id="video" class="rounded-lg" autoplay playsinline />
+</template>
