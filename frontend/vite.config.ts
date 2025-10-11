dbf90d4e29731f4f4480c6c4a9cca626436f255f
gitbani <banisomo2003@gmail.com>
2025-10-10 20:40:59 -0400

    Set up https

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,36 +1,42 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: false,
+  plugins: [
+    vue(), 
+    mkcert(), 
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'rtsl-frontend',
-      short_name: 'rtsl-frontend',
-      description: 'Frontend for rtsl, real-time (American) sign language translation',
-      theme_color: '#ffffff',
-    },
+      manifest: {
+        name: 'rtsl-frontend',
+        short_name: 'rtsl-frontend',
+        description: 'Frontend for rtsl, real-time (American) sign language translation',
+        theme_color: '#ffffff',
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      }
+    }),
+  ],
+  server: { https: true },
 })
