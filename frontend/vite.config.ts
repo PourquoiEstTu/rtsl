1077f14781ce67f38226a14bad268e833fea2686
gitbani <banisomo2003@gmail.com>
2025-10-10 23:14:49 -0400

    Add camera

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,16 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import type { PluginOption } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(), 
-    mkcert(), 
+    vue(),
+    mkcert(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: false,
@@ -37,10 +40,9 @@ export default defineConfig({
         type: 'module',
       }
     }),
-  ],
-  server: { 
-    https: true,
-    host: true,
+  ] as PluginOption[],
+  server: {
+    host: '0.0.0.0',
     port: 5173,
   },
 })
