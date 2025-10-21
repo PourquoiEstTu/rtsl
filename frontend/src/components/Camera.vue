ebfa6a7b4a0c50484937a267147d81ec46b167d4
HaNguyenCS <nguyenthanhha190903@gmail.com>
2025-10-20 22:54:43 -0400

    Fixing web logo & Add simple translation box

--- frontend/src/components/Camera.vue
+++ frontend/src/components/Camera.vue
@@ -2,6 +2,7 @@
 import { onMounted } from 'vue';
 import Button from '@/volt/Button.vue';
 import EyeIcon from '@primevue/icons/eye';
+import TranslationBox from '@/components/TranslationBox.vue'
 
 onMounted(async () => {
   const videoEl = document.getElementById('video') as HTMLMediaElement;
@@ -18,22 +19,30 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div class="relative w-dvw h-dvh overflow-hidden">
-    <video
-      id="video"
-      class="absolute top-0 left-0 w-full h-full object-cover bg-black"
-      autoplay
-      playsinline
-    />
-    <Button
-      :class="`
-        px-3! absolute! bottom-12 left-1/2 -translate-x-1/2 
-        border-4 border-white backdrop-blur-lg bg-transparent rounded-full! w-24 h-24
-        active:scale-95 transition
-      `"
-      size="large"
+  <div class="flex h-dvh w-dvw overflow-hidden bg-black">
+    <div
+      class="hidden md:flex w-48 bg-[#FFFC00]/80 items-center justify-center text-black font-semibold"
     >
-      <EyeIcon class="w-16 h-16 text-white" />
-    </Button>
+      MENU
+    </div>
+    <div class="relative flex-1 flex items-center justify-center">
+      <video
+        id="video"
+        class="absolute top-0 left-0 w-full h-full object-cover bg-black"
+        autoplay
+        playsinline
+      />
+      <Button
+        :class="`
+          px-3! absolute! bottom-12 left-1/2 -translate-x-1/2 
+          border-4 border-white backdrop-blur-lg bg-transparent rounded-full! w-24 h-24
+          active:scale-95 transition
+        `"
+        size="large"
+      >
+        <EyeIcon class="w-16 h-16 text-white" />
+      </Button>
+    </div>
+    <TranslationBox />
   </div>
 </template>
---
+++ frontend/src/components/TranslationBox.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// bind this with ML model output later
+const translation = ref('Waiting for sign input...')
+</script>
+
+<template>
+  <div
+    class="flex flex-col w-72 h-full bg-[#FFF5B8]/90 text-black justify-center items-center p-4 border-l border-yellow-300"
+  >
+    <h2 class="text-lg font-bold mb-2">Translation:</h2>
+    <p class="text-center text-base font-medium leading-relaxed">
+      {{ translation }}
+    </p>
+  </div>
+</template>
