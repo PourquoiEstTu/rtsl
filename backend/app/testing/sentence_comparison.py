879b671fa3baf29e132ba8f3fd52f4822cd1999d
Gaurav Sharma <gaurav53080@gmail.com>
2026-01-17 17:22:40 -0500

    added WER comparison

--- backend/app/testing/sentence_comparison.py
+++ backend/app/testing/sentence_comparison.py
@@ -1,12 +1,22 @@
 from sentence_transformers import SentenceTransformer
+from jiwer import wer
 
-def sentence_comparison(sentence1:str, sentence2:str):
+# returns close to 1 if two sentences are semantically similar
+def sentence_semantic_comparison(label:str, predicted:str):
     """ Compares two strings and returns a float number from 0 to 1 where 1 means the sentences have identical meaning through transformers.
-    Inputs: Two strings.
-    Output: Value between 0 to 1 representing how similar the sentences are.
+    Inputs: Label and predicted sentence.
+    Output: Value between 0 to 1 representing how similar the meanings are using cosine similarity.
     """
     model = SentenceTransformer("all-MiniLM-L6-v2")
-    embedding1 = model.encode(sentence1)
-    embedding2 = model.encode(sentence2)
-    similarity = model.similarity(embedding1, embedding2)
+    label_embedding = model.encode(label)
+    predicted_embedding = model.encode(predicted)
+    similarity = model.similarity(label_embedding, predicted_embedding)
     return similarity.item()
+
+# returns close to 0 if two sentences are syntactically similar
+def sentence_syntactic_comparison(label:str, predicted:str):
+    """ Compares two strings and returns the word error rate between the label and prediction. 
+    Inputs: Label and predicted sentence.
+    Output: Float that roughly represents the percentage of words incorrect in the label (can be greater than 1 for extremely bad predictions).
+    """
+    return wer(label, predicted)
