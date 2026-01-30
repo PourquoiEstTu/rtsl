3b4b42b929fa8474fe455a17f07f13113bc50523
PourquoiEstTu <quyumr@mcmaster.ca>
2026-01-30 15:42:47 -0500

    Remove dupe normalize_sequence_length() and add files checked counter to old one

--- backend/app/preprocess.py
+++ backend/app/preprocess.py
@@ -254,6 +254,7 @@ def normalize_sequence_length(input_dir: str, output_dir, overwrite=False):
        input_dir: directory with features generated from gen_videos_features()
        output_dir: directory where processed files are saved"""
 
+    files_checked_counter = 0
     max_length = 0
     for file in os.scandir(input_dir) :
         if file.name == "ordered_labels.npy":
@@ -262,6 +263,9 @@ def normalize_sequence_length(input_dir: str, output_dir, overwrite=False):
             arr = np.load(file.path)
             n_frames = arr.shape[0] # number of rows/frames
             max_length = max(max_length, n_frames)
+            files_checked_counter += 1
+            if files_checked_counter % 100 == 0 :
+                print(f"Maximum Number of frames after {files_checked_counter} files checked: {max_length}")
     print(f"[normalize_sequence_length] Max frame length found: {max_length}")
 
     for file in os.scandir(input_dir) :
@@ -299,6 +303,7 @@ def normalize_sequence_length(input_dir: str, output_dir, overwrite=False):
 # normalize_sequence_length(TRAIN_OUTPUT_DIR_CLEANED, TRAIN_OUTPUT_DIR_NORMALIZED, True)
 # normalize_sequence_length(VALIDATION_OUTPUT_DIR_CLEANED, VALIDATION_OUTPUT_DIR_NORMALIZED, True)
 # normalize_sequence_length(TEST_OUTPUT_DIR_CLEANED, TEST_OUTPUT_DIR_NORMALIZED, True)
+normalize_sequence_length("/u50/quyumr/archive/test_output_json_video", "/u50/quyumr/archive/test_output_json_video_padded", overwrite=True)
 
 def force_equal_dims_features_labels(input_dir: str, label_file: str, json_path: str = JSON_PATH, overwrite: bool = False) :
     """Make sure that X and y have the same dimensions. This function
@@ -558,7 +563,8 @@ def convert_keypoints_dir_to_video(input_dir: str, output_dir: str, overwrite_fi
             # vidwrite(f"{output_dir}/{file.name.strip('.json')}.avi", video)
             # print(f"{file.name.strip('.json')}.mp4 saved to {output_dir}")
     print(f"{fail_count} number of files failed to save")
-# convert_keypoints_dir_to_video("/u50/quyumr/archive/train_output_json", "/u50/quyumr/archive/train_output_json_video", True)
+# convert_keypoints_dir_to_video("/u50/quyumr/archive/validation_output_json", "/u50/quyumr/archive/validation_output_json_video", True)
+# 174 training files and 33 test files failed to save
 # print(hand_keypoint_to_img(f"{TRAIN_OUTPUT_DIR}/00335.json"))
 
 
@@ -632,57 +638,3 @@ def perpare_all_keypoint_files_for_resnet(input_dir: str, output_dir: str, overw
 # outputs torch.Size([44, 3, 224, 224])
 # 44 frames in video 
 # each frame is a 3x224x224 tensor
-
-
-# code taken from PR #36
-def normalize_sequence_length(input_dir: str, output_dir, overwrite=False):
-    """Normalize all feature files to have the same number of frames.
-       Pads or truncates all .npy feature arrays in input_dir so they all have
-    the same number of frames (rows). Uses the max length found across videos.
-       input_dir: directory with features generated from gen_videos_features()
-       output_dir: directory where processed files are saved"""
-
-    max_length = 0
-    for file in os.scandir(input_dir) :
-        if file.name == "ordered_labels.npy":
-            continue
-        if file.is_file() and file.name.endswith(".npy"):
-            arr = np.load(file.path)
-            # print("here")
-            n_frames = arr.shape[0] # number of rows/frames
-            max_length = max(max_length, n_frames)
-    print(f"[normalize_sequence_length] Max frame length found: {max_length}")
-    return
-
-    for file in os.scandir(input_dir) :
-        if file.is_file() and file.name.endswith(".npy"):
-            out_path = os.path.join(output_dir, file.name)
-            if not overwrite and os.path.exists(out_path):
-                continue
-            features = np.load(file.path)
-
-            if features.size == 0 or file.name == "ordered_labels.npy":
-                print(f"[WARNING] Skipping {file.name}: empty or invalid feature array (shape={features.shape}),(size={features.size})")
-                continue
-
-            n_frames, n_features = features.shape
-            if n_frames < max_length :
-                # pad with zeros
-                pad_len = max_length - n_frames
-                padded = np.vstack([
-                    features,
-                    np.zeros((pad_len, n_features), dtype=np.float32)
-                ])
-            elif n_frames > max_length:
-                # safety guard, should never enter this branch if data cleaning was done correctly
-                raise ValueError(
-                    f"[NormalizationError] Video '{file.name}' has {n_frames} frames, "
-                    f"which exceeds the expected maximum of {max_length}. "
-                    "This indicates that the dataset contains inconsistent feature lengths. "
-                    "Recheck your cleaning or max_length computation step."
-                )
-            else:
-                padded = features
-            np.save(out_path, padded)
-            print(f"Saved normalized features: {out_path}")
-normalize_sequence_length("/u50/quyumr/archive/train_output_json_video", "/u50/quyumr/archive/train_output_json_video_padded", overwrite=True)
